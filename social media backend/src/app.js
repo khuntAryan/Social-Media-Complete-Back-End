e4a4b69601a047db493deea450516f5cfcaa815f
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2024-08-31 23:29:34 +0530

    router config

--- social media backend/src/app.js
+++ social media backend/src/app.js
@@ -17,4 +17,16 @@ app.use(Express.urlencoded({
 app.use(Express.static("public"))
 app.use(cookieParser())
 
+// importing router 
+import routerUser from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", routerUser)
+// we are making these in sepereate files because if we want to access anything inside user 
+// such as login user or register user we can access all that once we entre users from here
+// so that we don't have to call users again and again 
+
+// you click one and can access all 
+
+
 export { app }
