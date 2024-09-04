b80355f61efbd8c3dd669bf1bfa3a019dc66a898
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2024-09-04 22:43:30 +0530

    Add subscription model

---
+++ social media backend/src/models/subscription.models.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    channel: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    }
+
+    // here both of them are user only
+}, { timestamps: true })
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
